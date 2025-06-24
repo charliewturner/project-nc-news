62d9a954074d0af6693db96758fa1559de38639b
charliewturner <c.w.turner@hotmail.com>
2025-06-24 12:22:36 +0100

    Add logs to attempt getAPI debgu

--- src/components/getAPI.jsx
+++ src/components/getAPI.jsx
@@ -1,8 +1,10 @@
 function getAPI(url) {
   return fetch(`${url}`).then((response) => {
     if (!response.ok) {
+      console.log("response not ok");
       return Promise.reject(response);
     }
+    console.log("response ok");
     return response.json();
   });
 }
