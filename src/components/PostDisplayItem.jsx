10358788ea258ea61ed438bf7640f2ea480a8ae6
charliewturner <c.w.turner@hotmail.com>
2025-06-24 13:24:40 +0100

    Continue editing CSS for visibilit

--- src/components/PostDisplayItem.jsx
+++ src/components/PostDisplayItem.jsx
@@ -13,27 +13,27 @@ function PostDisplayItem({
 }) {
   return (
     <li id={id} className="post-item">
-      <section>
+      <section className="post-item-left">
         <img
           className="post-image"
           src={article_img_url}
           alt="Image not found."
         />
       </section>
-      <section>
+      <section className="post-item-center">
         <p className="post-title">{title}</p>
         <p className="post-topic">{topic}</p>
-        <p className="post-date">{created_at}</p>
-        <p className="post-author">{author}</p>
+        <p className="post-date">{created_at} </p>
+        <p className="post-author">{author} </p>
         <button onClick={() => seeFullPost(id)}>
           {seeFullPost}See full post
         </button>
         <button onClick={() => shareLink(id)}>{shareLink}Share link</button>
       </section>
-      <section>
-        <div>upvote</div>
-        <div>{votes}</div>
-        <div>downvote</div>
+      <section className="post-item-right">
+        <div className="upvote">upvote</div>
+        <div className="voteNumber">{votes}</div>
+        <div className="downvote">downvote</div>
       </section>
     </li>
   );
