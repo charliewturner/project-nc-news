7654302ff0736d8ea4bf43a34b66e55794f104dd
charliewturner <c.w.turner@hotmail.com>
2025-06-24 12:20:44 +0100

    Amend getAPI to attempt error fix

--- src/components/getAPI.jsx
+++ src/components/getAPI.jsx
@@ -1,10 +1,7 @@
 function getAPI(url) {
   return fetch(`${url}`).then((response) => {
     if (!response.ok) {
-      return Promise.reject({
-        status: response.status,
-        msg: response.msg,
-      });
+      return Promise.reject(response);
     }
     return response.json();
   });
