8625ab22bb8180fa8a9ba96e2aa815021c5274fc
charliewturner <c.w.turner@hotmail.com>
2025-06-29 14:12:32 +0100

    Add dropdowns and sort order options on the homepage

--- src/components/FilterControls.jsx
+++ src/components/FilterControls.jsx
@@ -9,7 +9,7 @@ function FilterControls({
   setSortOrder,
 }) {
   return (
-    <section className="topic-selector">
+    <section className="filter-controls">
       <label htmlFor="topic-selector">Filter posts by topic: </label>
       <select
         className="topic-selector-dropdown"
@@ -21,10 +21,43 @@ function FilterControls({
           setTopicFiltered(value == "" ? null : value);
         }}
       >
-        <option value="">No selection</option>
+        <option value="">All topics</option>
         <option value="coding">Coding</option>
         <option value="football">Football</option>
         <option value="cooking">Cooking</option>
+      </select>
+
+      <label htmlFor="sort-by">Sort by: </label>
+      <select
+        className="sort-by-dropdown"
+        name="sort-by"
+        id="sort-by"
+        value={sortBy}
+        onChange={(element) => {
+          let value = element.target.value;
+          setSortBy(value == "" ? null : value);
+        }}
+      >
+        <option value="">No selection</option>
+        <option value="votes">Votes</option>
+        <option value="comment_count">Comments</option>
+        <option value="date">Date</option>
+      </select>
+
+      <label htmlFor="order-by">Order: </label>
+      <select
+        className="order-by-dropdown"
+        name="order-by"
+        id="order-by"
+        value={sortOrder}
+        onChange={(element) => {
+          let value = element.target.value;
+          setSortOrder(value == "" ? null : value);
+        }}
+      >
+        {" "}
+        <option value="desc">Desc</option>
+        <option value="asc">Asc</option>
       </select>
     </section>
   );
